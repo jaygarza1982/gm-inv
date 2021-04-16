e9dbe479cfb327477755a0f94bb871e89a0cd956
Jake <jaygarza1982@gmail.com>
2021-04-16 16:43:25 -0400

    Create csv-files dir on server start, fixed development react proxying.

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var fs = require('fs');
 
 var itemsRouter = require('./routes/api/items');
 
@@ -16,5 +17,14 @@ app.use('/api/items', itemsRouter);
 
 //Serve our static react app
 app.use(express.static('/app/gm-inv-web/build'));
+
+try {
+    if (!fs.existsSync('/app/csv-files')) {
+        fs.mkdirSync('/app/csv-files');
+    }
+}
+catch (err) {
+    console.log(`Could not create /app/csv-files. Reason: "${JSON.stringify(err)}"`)
+}
 
 module.exports = app;
